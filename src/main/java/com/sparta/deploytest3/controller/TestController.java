fd012c231279062ad34299d31cf9d662420c6479
Inventor <developer7098@gmail.com>
2023-01-01 16:21:33 +0900

    [test]#7 테스트 - 코드적용

--- src/main/java/com/sparta/deploytest3/controller/TestController.java
+++ src/main/java/com/sparta/deploytest3/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/")
     public String home(){
-        return "Hello World!";
+        return "Hello World! Test1";
     }
 }
