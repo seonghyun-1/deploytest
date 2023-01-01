56443b4c650a0058f6d2a5b9d114e79ce9eb412a
Inventor <developer7098@gmail.com>
2023-01-01 13:35:53 +0900

    [test]#2 테스트 코드 작성

---
+++ src/main/java/com/sparta/deploytest3/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.sparta.deploytest3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+}
