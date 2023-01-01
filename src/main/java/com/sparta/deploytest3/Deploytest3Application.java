4be6717a340951a74a554eaf1b9f3c766ae9bf71
Inventor <developer7098@gmail.com>
2023-01-01 13:29:10 +0900

    [test]#1깃허브-로컬브랜치 연동

---
+++ src/main/java/com/sparta/deploytest3/Deploytest3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.deploytest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploytest3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Deploytest3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/deploytest3/Deploytest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.deploytest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploytest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
